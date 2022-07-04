b5014e80006bae16c9e207a583a5344458b3657a
PhucLe <lehuuphuc23031999@gmail.com>
2022-07-04 15:43:28 +0700

    add logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { DOMMessage, DOMMessageResponse } from './types';
 
 function App() {
+  const [title, setTitle] = React.useState('');
+  const [headlines, setHeadlines] = React.useState<string[]>([]);
+
+  useEffect(() => {
+    chrome.tabs && chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, tabs => {
+      /**
+       * Sends a single message to the content script(s) in the specified tab,
+       * with an optional callback to run when a response is sent back.
+       *
+       * The runtime.onMessage event is fired in each content script running
+       * in the specified tab for the current extension.
+       */
+      console.log('tabs: ', tabs);
+      chrome.tabs.sendMessage(
+        tabs[0].id || 0,
+        { type: 'GET_DOM' } as DOMMessage,
+        (response: DOMMessageResponse) => {
+          console.log('response: ', response);
+          if (!chrome.runtime.lastError) {
+            setTitle(response.title);
+            setHeadlines(response.headlines);
+          } else {
+            console.log('loi roi!!!: ', chrome.runtime.lastError);
+          }
+
+        });
+    });
+  });
   return (
     <div className="App">
       <h1>SEO Extension built with React!</h1>
@@ -11,24 +42,26 @@ function App() {
         <li className="SEOValidation">
           <div className="SEOValidationField">
             <span className="SEOValidationFieldTitle">Title</span>
-            <span className="SEOValidationFieldStatus Error">
-              90 Characters
+            <span className={`SEOValidationFieldStatus ${title.length < 30 || title.length > 65 ? 'Error' : 'Ok'}`}>
+              {title?.length} Characters
             </span>
           </div>
           <div className="SEOVAlidationFieldValue">
-            The title of the page
+            {title}
           </div>
         </li>
 
         <li className="SEOValidation">
           <div className="SEOValidationField">
             <span className="SEOValidationFieldTitle">Main Heading</span>
-            <span className="SEOValidationFieldStatus Ok">
-              1
+            <span className={`SEOValidationFieldStatus ${headlines.length !== 1 ? 'Error' : 'Ok'}`}>
+              {headlines.length}
             </span>
           </div>
           <div className="SEOVAlidationFieldValue">
-            The main headline of the page (H1)
+            <ul>
+              {headlines.map((headline, index) => (<li key={index}>{headline}</li>))}
+            </ul>
           </div>
         </li>
       </ul>
